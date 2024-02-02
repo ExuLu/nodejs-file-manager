01803cde8361f5af4b21fd35718e35a413adca75
Alena Sharai <exultantislupus@gmail.com>
2024-02-02 17:26:53 +0400

    feat: create exit function and use it if user press ctrl + c or .exit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { stdin, stdout } from 'process';
 
 const args = process.argv.slice(2);
+function exitFromFileManager() {
+  stdout.write(`\nThank you for using File Manager, ${username}, goodbye!`);
+  process.exit();
+}
 
 const username = args.reduce(
   (acc, arg) =>
@@ -9,6 +13,10 @@ const username = args.reduce(
 );
 
 stdout.write(`Welcome to the File Manager, ${username} \n`);
-stdin.on('data', () => {
-    
+stdin.on('data', (data) => {
+  const stringData = data.toString().trim();
+  if (stringData === '.exit') exitFromFileManager();
+
+  console.log(stringData);
 });
+process.on('SIGINT', exitFromFileManager);
