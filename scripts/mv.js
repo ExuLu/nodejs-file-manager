e04a08cdd97de17a909514409d86f6c11ed632d7
Alena Sharai <exultantislupus@gmail.com>
2024-02-03 23:53:06 +0400

    refactor: change move command

--- scripts/mv.js
+++ scripts/mv.js
@@ -1,15 +1,64 @@
 import { join } from 'path';
 import infoAboutCurDir from './textInfo.js';
-import { cwd } from 'process';
-import { access, readdir, rm, stat } from 'fs/promises';
+import { access, rm } from 'fs/promises';
 import { createReadStream, createWriteStream } from 'fs';
 import addError from './error.js';
-import { noArguments } from './errMessages.js';
+import { noArguments, exist, wrongPath, notDir } from './errMessages.js';
 import createPath from './createPath.js';
-import copyCommand from './cp.js';
+import { pipeline } from 'stream/promises';
 
 export default async function moveCommand(userArg) {
-  copyCommand(userArg)
-    .then((res) => {})
-    .catch((err) => console.log(err));
+  const userArgArray = userArg.split(' ');
+
+  if (userArg.trim() === '' || userArgArray.length !== 2) {
+    addError('input', noArguments);
+    return;
+  }
+
+  const origFilePath = createPath(userArgArray[0]);
+  const dirPath = createPath(userArgArray[1]);
+  const fileName = origFilePath.slice(origFilePath.lastIndexOf('/') + 1);
+  const copyFilePath = join(dirPath, fileName);
+
+  let fileExist;
+
+  try {
+    await access(copyFilePath);
+    fileExist = true;
+  } catch (err) {
+    fileExist = false;
+  }
+
+  if (fileExist) {
+    addError('operation', exist);
+    return;
+  }
+
+  try {
+    await access(origFilePath);
+  } catch (err) {
+    addError('operation', wrongPath);
+    return;
+  }
+
+  const originalFileStream = createReadStream(origFilePath, 'utf-8');
+  const copyFileStream = createWriteStream(copyFilePath, 'utf-8');
+  try {
+    await pipeline(originalFileStream, copyFileStream);
+    infoAboutCurDir();
+  } catch (err) {
+    if (err.code === 'ENOTDIR') {
+      addError('input', notDir);
+      return;
+    } else if (err.code === 'ENOENT') {
+      addError('operation', wrongPath);
+      return;
+    } else {
+      console.log(err);
+      addError();
+      return;
+    }
+  }
+
+  await rm(origFilePath);
 }
