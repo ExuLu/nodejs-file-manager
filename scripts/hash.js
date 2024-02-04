c8e5d7d61c69445ad5a81878bc697a5413d9f5c4
Alena Sharai <exultantislupus@gmail.com>
2024-02-04 21:24:57 +0400

    feat: create hash command in separate module

--- scripts/hash.js
+++ scripts/hash.js
@@ -1,6 +1,17 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
 export default function hashCommand(userArg) {
-    if (userArg === '') {
-        addError('input', noArguments);
-      }
-      const filePath = createPath(userArg);
+  if (userArg === '') {
+    addError('input', noArguments);
+  }
+  const filePath = createPath(userArg);
+
+  try {
+    const fileContent = readFile(filePath, 'utf-8');
+    const hash = createHash('sha256').update(fileContent).digest('hex');
+    console.log(hash);
+  } catch (err) {
+    console.log(err);
+  }
 }
