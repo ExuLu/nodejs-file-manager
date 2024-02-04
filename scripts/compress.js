78e252e18ff98d7251d8e63a8ff0697e99e833b8
Alena Sharai <exultantislupus@gmail.com>
2024-02-04 22:26:33 +0400

    feat: create compress logic using brotli algorithm and streams

--- scripts/compress.js
+++ scripts/compress.js
@@ -1,6 +1,10 @@
+import { join } from 'path';
 import createPath from './createPath.js';
-import { noArguments } from './errMessages.js';
+import { noArguments, wrongPath } from './errMessages.js';
 import addError from './error.js';
+import { access } from 'fs/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createBrotliCompress } from 'zlib';
 
 export default async function compressCommand(userArg) {
   const userArgArray = userArg.split(' ');
@@ -13,8 +17,7 @@ export default async function compressCommand(userArg) {
   const origFilePath = createPath(userArgArray[0]);
   const dirPath = createPath(userArgArray[1]);
   const fileName = origFilePath.slice(origFilePath.lastIndexOf('/') + 1);
-  const archName = fileName.slice(fileName.lastIndexOf('.')) + '.br';
-  console.log(archName);
+  const archName = fileName.slice(0, fileName.lastIndexOf('.')) + '.br';
   const archPath = join(dirPath, archName);
 
   let fileExist;
@@ -36,5 +39,14 @@ export default async function compressCommand(userArg) {
   } catch (err) {
     addError('operation', wrongPath);
     return;
+  }
+
+  try {
+    const readStream = createReadStream(origFilePath, 'utf-8');
+    const writeStream = createWriteStream(archPath);
+    const brotli = createBrotliCompress();
+    readStream.pipe(brotli).pipe(writeStream);
+  } catch (err) {
+    console.log(err);
   }
 }
