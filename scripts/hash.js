3da65011b24c09706539983186be4c0c6f629c7f
Alena Sharai <exultantislupus@gmail.com>
2024-02-04 21:29:35 +0400

    feat: add error handling to hash function

--- scripts/hash.js
+++ scripts/hash.js
@@ -1,17 +1,28 @@
 import { createHash } from 'crypto';
 import { readFile } from 'fs/promises';
+import createPath from './createPath.js';
+import addError from './error.js';
+import { noArguments, notFile, wrongPath } from './errMessages.js';
 
-export default function hashCommand(userArg) {
+export default async function hashCommand(userArg) {
   if (userArg === '') {
     addError('input', noArguments);
   }
   const filePath = createPath(userArg);
 
   try {
-    const fileContent = readFile(filePath, 'utf-8');
+    const fileContent = await readFile(filePath, 'utf-8');
     const hash = createHash('sha256').update(fileContent).digest('hex');
     console.log(hash);
   } catch (err) {
-    console.log(err);
+    if (err.code === 'ENOENT') {
+      addError('operation', wrongPath);
+      return;
+    }
+    if (err.code === 'EISDIR') {
+      addError('input', notFile);
+      return;
+    }
+    addError();
   }
 }
