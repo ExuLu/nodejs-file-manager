ba02a1cc05f6472a1577f0129e242fd3c64207d5
Alena Sharai <exultantislupus@gmail.com>
2024-02-05 13:01:06 +0400

    feat: add message about entering commands to console

--- scripts/textInfo.js
+++ scripts/textInfo.js
@@ -2,4 +2,5 @@ import { stdout, cwd } from 'process';
 
 export default function infoAboutCurDir() {
   stdout.write(`You are currently in ${cwd()} \n`);
+  stdout.write('Please, enter your command here:\n');
 }
